70a7760533952c8bd6bd2e83a941261ae70a6cc5
Shivam Gautam <shivamgautama82@gmail.com>
2026-07-09 14:16:39 +0530

    Initial commit

---
+++ src/main/java/org/learingspring/upimesh/UpImeshApplication.java
@@ -0,0 +1,13 @@
+package org.learingspring.upimesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpImeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpImeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learingspring/upimesh/UpImeshApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learingspring.upimesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpImeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
